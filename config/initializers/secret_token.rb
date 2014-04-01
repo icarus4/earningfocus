01cddedc53c288f731b0918b0171b0d0a8cdeaf0
icarus4 <icarus4.chu@gmail.com>
2014-04-01 17:26:29 +0800

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Earningfocus::Application.config.secret_key_base = '0ffff2e6eac373ee674452b938dc4d5a4968c8de12266cbdfb0849d65c4437b6bc056a1de7dfade9bd3cf932f6be2e78e232ce3487d26ddcf0da96d3058910d3'
